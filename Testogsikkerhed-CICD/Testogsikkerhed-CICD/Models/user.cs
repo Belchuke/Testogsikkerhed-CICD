3f92f4f0e406a83e14071be0d88da4b01ecfb613
Belchuke <34919402+Belchuke@users.noreply.github.com>
2021-12-06 13:01:25 +0100

    DBCommit

---
+++ Testogsikkerhed-CICD/Testogsikkerhed-CICD/Models/user.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Testogsikkerhed_CICD.Models
+{
+    public class user
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Testogsikkerhed-CICD/Testogsikkerhed-CICD/Startup.cs
+++ Testogsikkerhed-CICD/Testogsikkerhed-CICD/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Testogsikkerhed_CICD.Models;
 
 namespace Testogsikkerhed_CICD
 {
@@ -26,7 +28,7 @@ namespace Testogsikkerhed_CICD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("default")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
